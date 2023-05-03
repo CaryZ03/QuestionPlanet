c52beab6ea7e2a286f60c00fff5337040b3a57df
yizhiqianbi <850587960@qq.com>
2023-05-04 02:01:45 +0800

    Nothing important

--- question_planet_vue/src/App.vue
+++ question_planet_vue/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul>
+        <li><router-link to="/">Home</router-link> </li>
+        <!-- <li><router-link to="/Menu">MenuView</router-link></li> -->
+        <li><router-link to="/login">login</router-link></li>
+        <li><router-link to="/register">register</router-link></li>
+      </ul>
     </nav>
     <router-view/>
   </div>
@@ -28,5 +32,30 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+</style>
+<style>
+#app nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: cadetblue ;
+}
+
+#app nav ul li {
+    float: left;
+}
+
+#app nav ul router-link {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+#app nav ul router-link:hover {
+    background-color: cornflowerblue;
 }
 </style>
--- question_planet_vue/src/router/index.js
+++ question_planet_vue/src/router/index.js
@@ -4,11 +4,21 @@ import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
+const About = () => import('../views/AboutView.vue')
+const Login = () => import('../views/LoginView.vue')
+const Register = () => import('../views/RegisterView.vue')
+const Menu = () => import('../views/MenuView.vue')
 const routes = [
+  
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/Menu',
+    name: 'Menu',
+    component: Menu
   },
   {
     path: '/about',
@@ -16,7 +26,18 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'Login' ,
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register' ,
+    component: Register
   }
 ]
 
