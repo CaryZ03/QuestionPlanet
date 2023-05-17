ba3229226f28a14619e01c00fffeb43e1f1f2ba6
caryz <Zhangkairui250@126.com>
2023-05-17 21:25:24 +0800

    usermodels

--- user/views.py
+++ user/views.py
@@ -15,16 +15,18 @@ from django.core.cache import cache
 
 def login_required(view_func):
     def wrapper(request, *args, **kwargs):
-        if request.session is not None:
-            return view_func(request, *args, **kwargs)
-        else:
+        if not request.session.items():
             return JsonResponse({'errno': 1002, 'msg': "未登录"})
+        elif request.session['id'] != json.loads(request.body).get('id'):
+            return JsonResponse({'errno': 1003, 'msg': "用户不一致"})
+        else:
+            return view_func(request, *args, **kwargs)
     return wrapper
 
 
 def not_login_required(view_func):
     def wrapper(request, *args, **kwargs):
-        if request.session is None:
+        if not request.session.items():
             return view_func(request, *args, **kwargs)
         else:
             return JsonResponse({'errno': 1003, 'msg': "已登录"})
@@ -48,18 +50,19 @@ def user_register(request):
     username = data_json['username']
     password1 = data_json['password1']
     password2 = data_json['password2']
-    email = data_json.get('email', ' ')
+    email = data_json.get('email')
     if not bool(re.match("^[A-Za-z0-9][A-Za-z0-9_]{2,20}$", str(username))):
-        return JsonResponse({'errno': 1003, 'msg': "用户名不合法"})
+        return JsonResponse({'errno': 1011, 'msg': "用户名不合法"})
     elif User.objects.filter(user_name=username).exists():
-        return JsonResponse({'errno': 1004, 'msg': "用户名已存在"})
+        return JsonResponse({'errno': 1012, 'msg': "用户名已存在"})
     elif password1 != password2:
-        return JsonResponse({'errno': 1002, 'msg': "两次输入的密码不同"})
+        return JsonResponse({'errno': 1013, 'msg': "两次输入的密码不同"})
     elif not bool(re.match('^(?=.*\\d)(?=.*[a-zA-Z]).{6,20}$', str(password1))):
-        return JsonResponse({'errno': 1005, 'msg': "密码不合法"})
+        return JsonResponse({'errno': 1014, 'msg': "密码不合法"})
     else:
         new_user = User.objects.create(user_name=username, user_password=password1, user_email=email)
-        return JsonResponse({'errno': 0, 'msg': "注册成功"})
+        new_user.save()
+        return JsonResponse({'errno': 0, 'msg': "注册成功", 'uid': new_user.user_id})
 
 
 @csrf_exempt
@@ -76,9 +79,9 @@ def user_login(request):
             request.session['role'] = 'user'
             return JsonResponse({'errno': 0, 'data': user, 'msg': "登录成功"})
         else:
-            return JsonResponse({'errno': 1007, 'msg': "密码错误"})
+            return JsonResponse({'errno': 1022, 'msg': "密码错误"})
     else:
-        return JsonResponse({'errno': 1008, 'msg': "用户不存在"})
+        return JsonResponse({'errno': 1021, 'msg': "用户不存在"})
 
 
 @csrf_exempt
@@ -95,9 +98,9 @@ def admin_login(request):
             request.session['role'] = 'admin'
             return JsonResponse({'errno': 0, 'data': admin, 'msg': "登录成功"})
         else:
-            return JsonResponse({'errno': 1007, 'msg': "密码错误"})
+            return JsonResponse({'errno': 1032, 'msg': "密码错误"})
     else:
-        return JsonResponse({'errno': 1009, 'msg': "管理员账号不存在"})
+        return JsonResponse({'errno': 1031, 'msg': "管理员账号不存在"})
 
 
 @csrf_exempt
@@ -110,12 +113,45 @@ def logout(request):
 
 @csrf_exempt
 @login_required
+@require_http_methods(['POST'])
+def cancel_account(request):
+    id = request.session['id']
+    user = User.objects.get(user_id=id)
+    user.delete()
+    return JsonResponse({'errno': 0, 'msg': "注销成功"})
+
+
+@csrf_exempt
+@login_required
+@require_http_methods(['POST'])
+def ban_user(request):
+    id = request.session['id']
+    user = User.objects.get(user_id=id)
+    user.status = 'banned'
+    user.save()
+    return JsonResponse({'errno': 0, 'msg': "用户封禁成功"})
+
+
+@csrf_exempt
+@login_required
+@require_http_methods(['POST'])
+def un_ban_user(request):
+    id = request.session['id']
+    user = User.objects.get(user_id=id)
+    user.status = 'free'
+    user.save()
+    return JsonResponse({'errno': 0, 'msg': "用户解封成功"})
+
+
+
+@csrf_exempt
+@login_required
 @require_http_methods(['GET'])
 def check_profile(request):
     data_json = json.loads(request.body)
     uid = data_json['id']
     user = User.objects.get(user_id=uid)
-    return JsonResponse({'error': 0, 'data': user, 'msg': '返回用户信息成功'})
+    return JsonResponse({'errno': 0, 'data': user, 'msg': '返回用户信息成功'})
 
 
 @csrf_exempt
@@ -125,7 +161,7 @@ def check_profile_admin(request):
     data_json = json.loads(request.body)
     adid = data_json['id']
     admin = Admin.objects.get(admin_id=adid)
-    return JsonResponse({'error': 0, 'data': admin, 'msg': '返回管理员信息成功'})
+    return JsonResponse({'errno': 0, 'data': admin, 'msg': '返回管理员信息成功'})
 
 
 @csrf_exempt
@@ -133,26 +169,26 @@ def check_profile_admin(request):
 @require_http_methods(['POST'])
 def change_profile(request):
     data_json = json.loads(request.body)
-    id = data_json['id']
+    id = request.session.get('id')
     username = data_json['username']
     password1 = data_json['password1']
     password2 = data_json['password2']
     mail = data_json.get('email', '')
-    if not bool(re.match("^[A-Za-z0-9][A-Za-z0-9_]{2,20}*$", str(username))):
-        return JsonResponse({'errno': 1003, 'msg': "用户名不合法"})
-    elif User.objects.filter(user_id!=id, user_name=username).exists():
-        return JsonResponse({'errno': 1004, 'msg': "用户名已存在"})
+    if not bool(re.match("^[A-Za-z0-9][A-Za-z0-9_]{2,20}$", str(username))):
+        return JsonResponse({'errno': 1071, 'msg': "用户名不合法"})
+    elif User.objects.filter(user_id__ne=id, user_name=username).exists():
+        return JsonResponse({'errno': 1072, 'msg': "用户名已存在"})
     elif password1 != password2:
-        return JsonResponse({'errno': 1002, 'msg': "两次输入的密码不同"})
+        return JsonResponse({'errno': 1073, 'msg': "两次输入的密码不同"})
     elif not re.match('^(?=.*\\d)(?=.*[a-zA-Z]).{6,20}$', str(password1)):
-        return JsonResponse({'errno': 1005, 'msg': "密码不合法"})
+        return JsonResponse({'errno': 1074, 'msg': "密码不合法"})
     else:
-        user = User.objects.get(id=id)
+        user = User.objects.get(user_id=id)
         user.user_name = username
         user.user_password = password1
         user.user_mail = mail
         user.save()
-        return JsonResponse({'error': 0, 'data': user, 'msg': '修改用户信息成功'})
+        return JsonResponse({'errno': 0, 'data': user, 'msg': '修改用户信息成功'})
 
 
 @csrf_exempt
@@ -163,37 +199,42 @@ def change_profile_admin(request):
     id = data_json['id']
     password1 = data_json['password1']
     password2 = data_json['password2']
-    if request.session.get('id') == id:
-        if password1 != password2:
-            return JsonResponse({'errno': 1002, 'msg': "两次输入的密码不同"})
-        elif not re.match('^(?=.*\\d)(?=.*[a-zA-Z]).{6,20}$', str(password1)):
-            return JsonResponse({'errno': 1005, 'msg': "密码不合法"})
-        else:
-            admin = Admin.objects.get(admin_id=id)
-            admin.password = password1
-            return JsonResponse({'error': 0, 'data': Admin, 'msg': '修改用户信息成功'})
+    mail = data_json.get('email', '')
+    if not bool(re.match("^[A-Za-z0-9][A-Za-z0-9_]{2,20}$", str(username))):
+        return JsonResponse({'errno': 1081, 'msg': "用户名不合法"})
+    elif User.objects.filter(user_id__ne=id, user_name=username).exists():
+        return JsonResponse({'errno': 1082, 'msg': "用户名已存在"})
+    elif password1 != password2:
+        return JsonResponse({'errno': 1083, 'msg': "两次输入的密码不同"})
+    elif not re.match('^(?=.*\\d)(?=.*[a-zA-Z]).{6,20}$', str(password1)):
+        return JsonResponse({'errno': 1084, 'msg': "密码不合法"})
     else:
-        return JsonResponse({'errno': 1011, 'msg': "该管理员未登录"})
+        user = User.objects.get(user_id=id)
+        user.user_name = username
+        user.user_password = password1
+        user.user_mail = mail
+        user.save()
+        return JsonResponse({'errno': 0, 'data': user, 'msg': '修改用户信息成功'})
 
 
 @csrf_exempt
 @login_required
 @require_http_methods(['GET'])
 def check_created_questionnaires(request):
-    data_json = json.loads(request.body)
-    id = data_json['id']
-    user = User.objects.get(id=id)
-    return JsonResponse({'error': 0, 'data': user, 'msg': '返回用户信息成功'})
+    id = request.session['id']
+    user = User.objects.get(user_id=id)
+    created_questionnaires = user.user_created_questionnaires.all()
+    return JsonResponse({'errno': 0, 'msg': '返回问卷列表成功', 'data': created_questionnaires})
 
 
 @csrf_exempt
 @login_required
 @require_http_methods(['GET'])
 def check_filled_questionnaires(request):
-    data_json = json.loads(request.body)
-    id = data_json['id']
-    user = User.objects.get(id=id)
-    return JsonResponse({'error': 0, 'data': user, 'msg': '返回用户信息成功'})
+    id = request.session['id']
+    user = User.objects.get(user_id=id)
+    filled_questionnaires = user.user_filled_questionnaires.all()
+    return JsonResponse({'errno': 0, 'msg': '返回问卷列表成功', 'data': filled_questionnaires})
 
 
 def send_simple_message(mail, code):
@@ -211,96 +252,43 @@ def unsupported_request(request):
     return JsonResponse({'errno': 1001, 'msg': "请求方式错误"})
 
 
+@csrf_exempt
+@not_login_required
+@require_http_methods(['POST'])
+def send_verification_code(request):
+    data_json = json.loads(request.body)
+    username = data_json['username']
+    if User.objects.filter(user_name=username).exists():
+        user = User.objects.get(user_name=username)
+        email = user.user_email
+        if email:
+            code = randint(100000, 999999)
+            response = send_simple_message(email, str(code))
+            if response.status_code == 200:
+                return JsonResponse({'errno': 0, 'msg': '邮件发送成功', 'code': code})
+            return JsonResponse({'errno': 1012, 'msg': '邮箱发送失败'})
+        return JsonResponse({'errno': 1014, 'msg': '邮箱不存在'})
+    else:
+        return JsonResponse({'errno': 1008, 'msg': "用户不存在"})
 
-#
-# @csrf_exempt
-# @require_http_methods(['POST'])
-# def update_pass_user_step1(request):
-#     if request.session == None:
-#         data_json = json.loads(request.body)
-#         username = data_json['username']
-#         if User.objects.filter(username=username).exists() == True:
-#             user = User.objects.get(username=username)
-#             mail = user.mail
-#             if mail:
-#                 code = randint(100000, 999999)
-#                 cache.set(mail, str(code), 300)
-#                 response = send_simple_message(mail, str(code))
-#                 if response.status_code == 200:
-#                     return JsonResponse({'errno': 200, 'msg': '邮件发送成功'})
-#                 return JsonResponse({'errno': 1012, 'msg': '邮箱发送失败'})
-#             return JsonResponse({'errno': 1014, 'msg': '邮箱不存在'})
-#         else:
-#             return JsonResponse({'errno': 1008, 'msg': "用户不存在"})
-#     else:
-#         return JsonResponse({'errno': 1006, 'msg': "已有用户登录"})
-#
-#
-# @csrf_exempt
-# @require_http_methods(['POST'])
-# def update_pass_user_step2(request):
-#     data_json = json.loads(request.body)
-#     username = data_json['username']
-#     verification_code = data_json['verification_code']
-#     if User.objects.filter(username=username).exists() == True:
-#         user = User.objects.get(username=username)
-#         mail = user.mail
-#         if verification_code:
-#             code = cache.get(mail)
-#         if code and code == verification_code:
-#             return JsonResponse({'errno': 0, 'msg': '验证成功'})
-#         else:
-#             return JsonResponse({'error': 1013, 'msg': '验证码错误'})
-#     else:
-#         return JsonResponse({'errno': 1008, 'msg': "用户不存在"})
-#
-#
-# @csrf_exempt
-# @require_http_methods(['POST'])
-# def update_pass_user_step3(request):
-#     data_json = json.loads(request.body)
-#     username = data_json['username']
-#     password1 = data_json['password1']
-#     password2 = data_json['password2']
-#     if User.objects.filter(username=username).exists() == True:
-#         user = User.objects.get(username=username)
-#         if password1 != password2:
-#             return JsonResponse({'errno': 1002, 'msg': "两次输入的密码不同"})
-#         elif not re.match('^(?=.*\\d)(?=.*[a-zA-Z]).{6,20}$', str(password1)):
-#             return JsonResponse({'errno': 1005, 'msg': "密码不合法"})
-#         else:
-#             user.password = password1
-#             return JsonResponse({'errno': 0, 'msg': "重置密码成功"})
-#     else:
-#         return JsonResponse({'errno': 1008, 'msg': "用户不存在"})
-#
-#
-# @csrf_exempt
-# @require_http_methods(['GET'])
-# def show_ticket(request):
-#     data_json = json.loads(request.body)
-#     id = data_json['id']
-#     method = data_json['method']
-#     if request.session != id:
-#         if method == 0:
-#             if Ticket.objects.filter(userid=id, status='doing').exists() == True:
-#                 tickets = Ticket.objects.filter(userid=id, status='doing')
-#                 return JsonResponse({'error': 0, 'data': tickets, 'msg': '查询机票成功'})
-#             else:
-#                 return JsonResponse({'error': 1015, 'data': tickets, 'msg': '查询机票为空'})
-#         if method == 1:
-#             if Ticket.objects.filter(userid=id, status='end').exists() == True:
-#                 tickets = Ticket.objects.filter(userid=id, status='end')
-#                 return JsonResponse({'error': 0, 'data': tickets, 'msg': '查询机票成功'})
-#             else:
-#                 return JsonResponse({'error': 1015, 'data': tickets, 'msg': '查询机票为空'})
-#         if method == 2:
-#             if Ticket.objects.filter(userid=id, status='false').exists() == True:
-#                 tickets = Ticket.objects.filter(userid=id, status='false')
-#                 return JsonResponse({'error': 0, 'data': tickets, 'msg': '查询机票成功'})
-#             else:
-#                 return JsonResponse({'error': 1015, 'data': tickets, 'msg': '查询机票为空'})
-#         else:
-#             return JsonResponse({'error': 1016, 'data': tickets, 'msg': 'method 不合法'})
-#     else:
-#         return JsonResponse({'errno': 1011, 'msg': "该用户未登录"})
+
+@csrf_exempt
+@not_login_required
+@require_http_methods(['POST'])
+def reset_password(request):
+    data_json = json.loads(request.body)
+    username = data_json['username']
+    password1 = data_json['password1']
+    password2 = data_json['password2']
+    if User.objects.filter(user_name=username).exists():
+        user = User.objects.get(user_name=username)
+        if password1 != password2:
+            return JsonResponse({'errno': 1002, 'msg': "两次输入的密码不同"})
+        elif not re.match('^(?=.*\\d)(?=.*[a-zA-Z]).{6,20}$', str(password1)):
+            return JsonResponse({'errno': 1005, 'msg': "密码不合法"})
+        else:
+            user.password = password1
+            user.save()
+            return JsonResponse({'errno': 0, 'msg': "重置密码成功"})
+    else:
+        return JsonResponse({'errno': 1008, 'msg': "用户不存在"})
--- 文档/errors.md
+++ 文档/errors.md
@@ -1,3 +1,105 @@
 ## 101 user_register
 
-### 1011:
+请求类型：POST
+
+输入数据：
+
+```json
+{
+    "username": "username",
+    "password1": "password1",
+    "password2": "password2",
+    "email": "email"
+}
+```
+
+返回数据：
+
+```json
+{
+    "uid": uid
+}
+```
+
+错误码：
+
+1011：用户名不合法
+
+1012：用户名已存在
+
+1013：两次输入的密码相同
+
+1014：密码不合法
+
+0：注册成功
+
+## 102 user_login
+
+请求类型：POST
+
+输入数据：
+
+```
+{
+    "username": "username",
+    "password": "password"
+}
+```
+
+1021：用户不存在
+
+1022：密码错误
+
+0：登录成功
+
+103 admin_login
+
+1031：管理员账号不存在
+
+1032：密码错误
+
+0：登录成功
+
+104 logout
+
+0：登出成功
+
+105 check_profile
+
+0：返回用户信息成功
+
+106 check_profile_admin
+
+0：返回管理员信息成功
+
+107 change_profile
+
+1071：用户名不合法
+
+1072：用户名已存在
+
+1073：两次输入的密码相同
+
+1074：密码不合法
+
+0：修改用户信息成功
+
+108 change_profile_admin
+
+1081：用户名不合法
+
+1082：用户名已存在
+
+1083：两次输入的密码相同
+
+1084：密码不合法
+
+0：修改用户信息成功
+
+109 check_created_questionnaires
+
+0：返回问卷列表成功
+
+110 check_filled_questionnaires
+
+0：返回问卷列表成功
