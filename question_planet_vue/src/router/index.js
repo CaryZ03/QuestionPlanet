db18028f556c4cd872ac581fa0546816515456e3
yizhiqianbi <850587960@qq.com>
2023-05-19 00:52:20 +0800

    加入了好几个模块，使用CSS Pro优化工作流

--- question_planet_vue/src/router/index.js
+++ question_planet_vue/src/router/index.js
@@ -5,6 +5,7 @@ import HomeView from '../views/HomeView.vue'
 const Login = () => import('../views/LoginView.vue');
 const Register = () => import('../views/RegisterView.vue');
 const CreateQuestionnaireView = () => import('../views/CreateView.vue');
+const UserInfoView = () => import('../views/UserInfoView.vue')
 Vue.use(VueRouter)
 
 const routes = [
@@ -63,6 +64,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component:Register
+  },
+  {
+    path: '/userInfo',
+    name: 'UserInfo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component:UserInfoView
   }
 ]
 
