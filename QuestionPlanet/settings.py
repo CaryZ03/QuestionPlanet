5f7c009f59ea47e0c52f9f318b20745ec67f0215
caryz <Zhangkairui250@126.com>
2023-05-24 21:30:00 +0800

    cors

--- QuestionPlanet/settings.py
+++ QuestionPlanet/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'user',
     'questionnaire',
     'data',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,5 @@ MEDIA_ROOT = '/media/'
 MEDIA_URL = '/media/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
