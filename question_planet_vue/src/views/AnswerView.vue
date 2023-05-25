6f94023330ca6f01701a04c96e949490dabcbd50
XenonGa <111100012+XenonGa@users.noreply.github.com>
2023-05-25 11:05:08 +0800

    1

--- question_planet_vue/src/views/AnswerView.vue
+++ question_planet_vue/src/views/AnswerView.vue
@@ -165,17 +165,6 @@ export default {
             };
             const jsonString = JSON.stringify(dataObject);
             console.log(jsonString);
-            /*
-            const filename = 'data.json';
-
-            const blob = new Blob([jsonString], { type: 'application/json' });
-            const url = URL.createObjectURL(blob);
-            
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = filename;
-            link.click();
-            URL.revokeObjectURL(url);*/
 
         },
 
