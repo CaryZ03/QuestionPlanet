0626e03c750cbbad8ce15580a05c674c1b815b73
caryz <Zhangkairui250@126.com>
2023-06-04 17:18:06 +0800

    i

--- user/views.py
+++ user/views.py
@@ -356,4 +356,4 @@ def change_user_status(request):
 @csrf_exempt
 @require_http_methods(['POST'])
 def deploy_test(request):
-    return JsonResponse({'errno': 0, 'ver': "5", 'cur_time': now()})
+    return JsonResponse({'errno': 0, 'ver': "6", 'cur_time': now()})
